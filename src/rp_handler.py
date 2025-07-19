f63dfaa7fdadd65d02f9f6cd56bddd0a0690782b
Ilia Peterov <ipeterov1@gmail.com>
2025-07-19 14:54:29 +0400

    Fix accidentally adding extra args to initial upload_images

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -315,7 +315,7 @@ def handler(job):
     )
 
     # Upload images if they exist
-    upload_result = upload_images(images, job)
+    upload_result = upload_images(images)
 
     if upload_result["status"] == "error":
         return upload_result
